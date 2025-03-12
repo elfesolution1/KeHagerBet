8f787cd12b667e9365ef30a06eda12abe4823a20
elfesolution1 <elfesolution1@gmail.com>
2025-03-12 14:05:19 +0300

    Update authControllers.js
    Revert changes

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -13,34 +13,34 @@ const {
   sendSellerVerificationEmail,
 } = require("../utils/sendEmail");
 class authControllers {
- admin_login = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const admin = await adminModel.findOne({ email }).select("+password");
-    if (admin) {
-      const match = await bcrypt.compare(password, admin.password);
-      if (match) {
-        const token = await createToken({
-          id: admin.id,
-          role: admin.role,
-        });
-        res.cookie("accessToken", token, {
-          expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production", // Ensure secure flag is set in production
-          sameSite: "strict",
-        });
-        responseReturn(res, 200, { token, message: "Login success" });
+admin_login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const admin = await adminModel.findOne({ email }).select("+password");
+      if (admin) {
+        const match = await bcrypt.compare(password, admin.password);
+        if (match) {
+          const token = await createToken({
+            id: admin.id,
+            role: admin.role,
+          });
+          res.cookie("accessToken", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+          });
+          responseReturn(res, 200, { token, message: "Login success" });
+        } else {
+          responseReturn(res, 404, { error: "Password wrong" });
+        }
       } else {
-        responseReturn(res, 404, { error: "Password wrong" });
+        responseReturn(res, 404, { error: "Email not found" });
       }
-    } else {
-      responseReturn(res, 404, { error: "Email not found" });
+    } catch (error) {
+      responseReturn(res, 500, { error: error.message });
     }
-  } catch (error) {
-    responseReturn(res, 500, { error: error.message });
-  }
-};
+  };
 
   admin_register = async (req, res) => {
     const { email, name, password } = req.body;
